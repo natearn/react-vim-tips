126df5c96d4feb04c8477b3db2a0849f23214ff4
Nathan Arnold <natearn@natearn.info>
2017-12-13 11:46:52 +0100

    Initialize project
    I copied and modified the configs from one of my other projects,
    but it will mae sense soon!

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# change these for an easier experience
+IMAGE_NAME="natearn/react-vim-tips"
+CONTAINER="rvt-dev"
+COMMAND="sh"
+
+if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]];
+then docker build -t $IMAGE_NAME .
+fi
+
+if docker inspect -f '{{.State.Running}}' "$CONTAINER" &> /dev/null
+then docker exec -it "$CONTAINER" "$COMMAND"
+else docker run -it --rm --name "$CONTAINER" \
+  -v $(pwd):/home/node/app \
+  -v /home/node/app/node_modules \
+  "$IMAGE_NAME" \
+  "$COMMAND"
+fi
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js','.jsx'],
+    modules: [path.resolve(__dirname, 'src'),'node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              camelCase: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+}
