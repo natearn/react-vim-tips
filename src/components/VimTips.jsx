dfa8d8a794428a56b6a559cd76dd2b28f750dfd0
Nathan Arnold <natearn@natearn.info>
2017-12-18 20:18:32 +0100

    Select a new random tip every 10 seconds
    Again, this is as naive as I could think to make it. Promises are
    good enough for this problem, but as things get more complicated,
    I think I'll be moving to an observable library.

--- src/components/VimTips.jsx
+++ src/components/VimTips.jsx
@@ -12,10 +12,25 @@ class VimTips extends React.Component {
 			}],
 			index: 0
 		}
-		fetchTips().then(tips => this.setState({
-			tips: tips,
-			index: Math.floor(Math.random() * tips.length),
-		}))
+		fetchTips()
+			.then(tips => this.setState({ tips: tips }))
+			.then(() => this.setRandomTip())
+			.then(() => this.every(10000,() => this.setRandomTip()))
+	}
+
+	setRandomTip() {
+		this.setState({
+			index: Math.floor(Math.random() * this.state.tips.length)
+		})
+	}
+
+	every(interval,action) {
+		const delay = new Promise(resolve => {
+			setTimeout(resolve,interval)
+		})
+		return delay
+			.then(action)
+			.then(() => this.every(interval,action))
 	}
 
 	render() {
